72812418b21e16aab732891c3a5aa5f56a655305
Arthasee <baptiste.braun.delvoye@gmail.com>
2025-05-09 21:49:44 +0200

    type my ideas for obstacle_manager

--- bolide_obstacle_management/bolide_obstacle_management/obstacle_manager.py
+++ bolide_obstacle_management/bolide_obstacle_management/obstacle_manager.py
@@ -3,17 +3,22 @@ from rclpy.node import Node
 from std_msgs.msg import String, Bool
 
 # WORK IN PROGRESS - NEVER BEEN TESTED AND NOT FINISH AND NEED TO BE FIX
+
+# We can may be see it like a process of front data of the lidar (maybe a 15° zone in front of the car) to check obstacle and where to go if needed
+# If it can work with our controller it will be perfect for a beginning of a full autonomous car with obstacle avoidance
 class ObstacleManager(Node):
 
     def __init__(self):
         super().__init__('obstacle_manager')
 
         # OUBLISHER AND SUBSCRIBER
+        # TODO - change this sub in a pub to send it to cmd_vel_node or cmd_dir_node
         self.subscription = self.create_subscription(Bool, '/obstacle_detected', self.obstacle_callback, 10)
 
-        self.publisher_ = self.create_publisher(String, '/cmd_dir', 10)
+        self.publisher_ = self.create_publisher(String, '/cmd_dir', 10) # TODO - Why direction if after is forward or backward and a String message
 
     def obstacle_callback(self, msg):
+        # TODO - rework here needed
         if msg.data:
             self.get_logger().info('Obstacle détecté : on recule')
             command = String()
@@ -29,6 +34,7 @@ def main(args=None):
     rclpy.init(args=args)
     node = ObstacleManager()
     rclpy.spin(node)
+    # will just destroy node here 
     node.destroy_node()
     rclpy.shutdown()
 
